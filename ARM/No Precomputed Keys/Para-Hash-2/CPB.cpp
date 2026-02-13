123df7691780e4b1543ff32a40678de0325ea193
AlexPs <AlexPs06@github.com>
2026-02-13 16:18:30 -0600

    ordene el codigo falta comentarios

--- ARM/No Precomputed Keys/Para-Hash-2/CPB.cpp
+++ ARM/No Precomputed Keys/Para-Hash-2/CPB.cpp
@@ -13,13 +13,56 @@
 #include <ctime>
 #include <iomanip>
 #include <string>
-// #include <sys/sysctl.h>
 #include <sstream>
 
-#define tag_size 64
-extern void ParaHash_V3(
-    const uint8_t* input, uint8_t* tag, uint8x16_t *roundKeys_1, const uint64_t lenght
+#define tag_size 32
+extern void ParaHash(
+    const uint8_t* input, uint8_t* tag, uint8x16_t *roundKeys, const uint64_t lenght
 );
+
+
+
+#if defined(__APPLE__)
+    #include <sys/types.h>
+    #include <sys/sysctl.h>
+#elif defined(__linux__)
+    #include <fstream>
+#endif
+
+std::string get_cpu_name() {
+
+#if defined(__APPLE__)
+
+    char buffer[256];
+    size_t size = sizeof(buffer);
+
+    if (sysctlbyname("machdep.cpu.brand_string", buffer, &size, nullptr, 0) == 0) {
+        return std::string(buffer);
+    }
+    return "Unknown Apple CPU";
+
+#elif defined(__linux__)
+
+    std::ifstream cpuinfo("/proc/cpuinfo");
+    std::string line;
+
+    while (std::getline(cpuinfo, line)) {
+        if (line.find("model name") != std::string::npos ||
+            line.find("Model") != std::string::npos) {
+
+            auto pos = line.find(':');
+            if (pos != std::string::npos)
+                return line.substr(pos + 2);
+        }
+    }
+    return "Unknown ARM CPU";
+
+#else
+
+    return "Unsupported Platform";
+
+#endif
+}
 
 void write_tag_hex(std::ofstream& file, const uint8_t* tag, size_t len = 64) {
     file << "Tag: ";
@@ -34,21 +77,7 @@ void write_tag_hex(std::ofstream& file, const uint8_t* tag, size_t len = 64) {
 }
 
 
-std::string get_cpu_name() {
-    std::ifstream cpuinfo("/proc/cpuinfo");
-    std::string line;
 
-    while (std::getline(cpuinfo, line)) {
-        if (line.find("model name") != std::string::npos ||
-            line.find("Model") != std::string::npos) {
-
-            auto pos = line.find(':');
-            if (pos != std::string::npos)
-                return line.substr(pos + 2);
-        }
-    }
-    return "Unknown CPU";
-}
 
 std::string get_compiler_info() {
 #ifdef __clang__
@@ -115,26 +144,52 @@ std::string get_datetime() {
 
 int main(int argc, char **argv) {
 
-    if ((argc < 1) || (argc > 2)) {
-		printf("Usage: [output_filename]\n");
+    if ((argc < 2) || (argc > 3)) {
+		printf("Usage: ./test [base_frequency_GHz] [output_filename]\n");
 		return 0;
 	} 
-    constexpr double CPU_FREQ = 2.4e9; // Apple M1 ≈ 3.2 GHz
+    
+    double CPU_FREQ = std::atof(argv[1]) * 1.e9; // Apple M1 ≈ 3.2 GHz
     constexpr int ITER = 100000;
+    std::ofstream file(argv[2]);
 
 
-
-    std::ofstream file(argv[1]);
-
-    file << "ParaHash CarryLess\n";
+    file << "Para-Hash-2 polinomial reduction\n";
     file << get_cpu_name() << "\n";
     file << get_compiler_info() << " (" << get_cpp_standard() << ")\n";
     file << get_arch_info() << "\n";
     file << "Run " << get_datetime() << "\n\n";
 
 
-    #if !defined(__ARM_FEATURE_CRYPTO)
-    file << "WARNING: Binary compiled without ARM crypto extensions!\n\n";
+    #if defined(__aarch64__) || defined(__arm__)
+        file << "Architecture: ARM\n";
+
+    #if defined(__ARM_FEATURE_CRYPTO)
+        file << "ARM Crypto Extensions: ENABLED\n";
+    #else
+        file << "ARM Crypto Extensions: DISABLED\n";
+    #endif
+
+    #elif defined(__x86_64__) || defined(__i386__)
+        file << "Architecture: x86\n";
+
+        #if defined(__AVX2__)
+            file << "AVX2: ENABLED\n";
+        #else
+            file << "AVX2: DISABLED\n";
+        #endif
+
+        #if defined(__AES__)
+            file << "AES-NI: ENABLED\n";
+        #else
+            file << "AES-NI: DISABLED\n";
+        #endif
+
+        #if defined(__PCLMUL__)
+            file << "PCLMULQDQ: ENABLED\n";
+        #else
+            file << "PCLMULQDQ: DISABLED\n";
+        #endif
     #endif
 
     // Dummy round keys
@@ -200,10 +255,10 @@ int main(int argc, char **argv) {
         start = std::chrono::steady_clock::now();
 
         for (int it = 0; it < ITER; it++) {
-            ParaHash_V3(
+            ParaHash(
                 msg.data(),
                 output,
-                obtained_keys,
+                roundKeys,
                 size
             );
 
